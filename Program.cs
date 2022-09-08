3ebebc9792e229b7dd04f3318efc1ef71e2c2ced
RodrigoSerratoGomez <s_o_a_d@hotmail.com>
2022-09-07 22:39:12 -0500

    Commit 02 PROYECTO

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using HeladeriaTAMS.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("PostgresSQLConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
